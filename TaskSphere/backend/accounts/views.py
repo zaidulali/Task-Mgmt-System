d17e41837a181ec83930be91756eb4708a015f3b
zaidulali <zaidulalimohammed@gmail.com>
2026-06-13 11:21:33 +0530

    Implemented user logout API

--- TaskSphere/backend/accounts/views.py
+++ TaskSphere/backend/accounts/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserRegistrationSerializer
@@ -22,3 +25,15 @@ class UserRegistrationView(generics.CreateAPIView):
                 status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Successfully logged out."}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response({"error": "Invalid or missing token."}, status=status.HTTP_400_BAD_REQUEST)
