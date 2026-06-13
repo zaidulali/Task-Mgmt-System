de09fdf78d16818dbc874ff17efb4f17d8756d41
zaidulali <zaidulalimohammed@gmail.com>
2026-06-13 11:56:37 +0530

    Implemented task CRUD APIs

--- TaskSphere/backend/tasks/views.py
+++ TaskSphere/backend/tasks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
+from .permissions import IsOwner
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
